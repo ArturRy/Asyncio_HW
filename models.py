912c65692fbe68894351d098de9436f479111440
Artur <mini--me@mail.ru>
2024-02-19 12:14:17 +0300

    commit

---
+++ models.py
@@ -0,0 +1,39 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+from sqlalchemy.orm import DeclarativeBase
+
+from sqlalchemy import Integer, Column, String, Text
+import os
+
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "secret")
+POSTGRES_USER = os.getenv("POSTGRES_USER", "swapi")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "swapi")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5431")
+
+PG_DSN = f'postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
+
+engine = create_async_engine(PG_DSN)
+Session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class SwapiPeople(Base):
+    __tablename__ = 'swapi_people'
+    id = Column(Integer, primary_key=True)
+    birth_year = Column(String)
+    eye_color = Column(String)
+    films = Column(Text)
+    gender = Column(String)
+    hair_color = Column(String)
+    height = Column(String)
+    homeworld = Column(String)
+    mass = Column(String)
+    name = Column(String)
+    skin_color = Column(String)
+    species = Column(Text)
+    starships = Column(Text)
+    vehicles = Column(Text)
